ac6e5c3bbc69e783a64c3a3afe597a02542dd532
Davide Farioli <davidefa95@hotmail.it>
2020-05-31 18:35:46 +0200

    Data join and click

--- js/main.js
+++ js/main.js
@@ -1,131 +1,99 @@
-/**
- * Created by vdidonato on 3/24/14.
- * Updated to v5 by titto on 4/22/20.
- */
+// 1 - Rappresenting data table in Js
 
-var delayTime = 1000, // time between the picture of one year and the next
-    updateTime = 500; // time for transitions
+// Selection of all svg of the page
+const svg = d3.select('svg');
 
-var margin = {top: 20, right: 20, bottom: 30, left: 40};
+const width = +svg.attr('width'); 
+const height = +svg.attr('height');
 
-// screen is 800 x 300
-// actual drawing leaves a margin around
-// width and height are the size of the actual drawing
-//
-var width = 800 - margin.left - margin.right;
-var height = 300 - margin.top - margin.bottom;
+const margin = { top: 20, right: 20, bottom: 20, left: 100, };
+const innerWidth = width - margin.left - margin.right;
+const innerHeight = height - margin.top - margin.bottom;
 
-// x is the scale for x-axis
-// domain is not given here but it is updated by updateXScaleDomain()
-// 
-var x = d3.scaleBand()         // ordinal scale
-    .rangeRound([10, width])   // leaves 10 pixels for the y-axis
-	.padding(.1);              // between the bands
-                               // x.bandwidth() will give the width of each band
+const g = svg.append('g')
+    .attr('transform', `translate(${margin.left} , ${margin.top})`);
 
-// y is the scale for y-axis
-// domain is not given here but it is updated by updateYScaleDomain()
-//
-var y = d3.scaleLinear().range([height, 0]);
+let xScale;
+let yScale;
 
-var xAxis = d3.axisBottom(x);  			// Bottom = ticks below
-var yAxis = d3.axisLeft(y).ticks(10);   // Left = ticks on the left 
+const circleRadius = 10;
 
-var svg = d3.select(".graph").append("svg")
-    .attr("width", width + margin.left + margin.right)     // i.e., 800 again 
-    .attr("height", height + margin.top + margin.bottom)   // i.e., 300 again
-    .append("g")                                           // g is a group
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");                                                    
+let datasets = [];
 
-// Parameter data is the object containing the values for a specific year
-// it has two fields: data.year (a number) and data.ageGroups (an array).
-// Each element d of data.ageGroups[] has d.ageGroup (for example "0-4") and  
-// d.population (a number)
-//
-function updateXScaleDomain(data) {
-    x.domain(data.map(function(d) { return d.x}));
-    // for example x.domain is initialized with ["0-4", "5-9", "10-14", ... ] 
-}
+const setDomainAndAxis = data => {
 
-function updateYScaleDomain(data){
-    y.domain([0, d3.max(data, function(d) { return d.y; })]);
-}
+    // 3 - Creating  linear scale: mapping del dominio (Data Space) -> Range (Pixel Scale)
+    xScale = d3.scaleLinear()
+        .domain([0, d3.max(data, d => d.x)])
+        .range([0, innerWidth]);
 
-function updateAxes(){
-    // ".y.axis" selects elements that have both classes "y" and "axis", that is: class="y axis"
-    svg.select(".y.axis").transition().duration(updateTime).call(yAxis);
-    svg.select(".x.axis").transition().duration(updateTime).call(xAxis);
-}
+    // Test domain
+    console.log("Domain for x: " + xScale.domain() + "\n Range for x:" +xScale.range());
 
-function drawAxes(){
+    yScale = d3.scaleLinear()
+        .domain([0, d3.max(data, d => d.y)])
+        .range([innerHeight, 0])
+    // Test domain
+    console.log("Domain for y: " + yScale.domain() + "\n Range for y:" +yScale.range());
 
-    // draw the x-axis
-    //
-    svg.append("g")
+    g.append('g')
         .attr("class", "x axis")
-        .attr("transform", "translate(0," + height + ")")
-        .call(xAxis);
+        .attr('transform', `translate(0 , ${innerHeight})`)
+        .call(d3.axisBottom(xScale));
 
-    // draw the y-axis
-    //
-    svg.append("g")
+    g.append('g')
         .attr("class", "y axis")
-        .call(yAxis);
+        .call(d3.axisLeft(yScale));
 
 }
 
-// Parameter data is the object containing the values for a specific year
-// it has two fields: data.year (a number) and data.ageGroups (an array).
-// Each element d of data.ageGroups[] has d.ageGroup (for example "0-4") and  
-// d.population (a number)
-//
-function updateDrawing(data){
+const updateDomainAndAxis = data => {
 
-    let x = data.x;
-    let y = data.y;
-
-    // Data join: function(d) is the key to recognize the right bar
-    
-    let circles = svg.selectAll(".circle").data(data, function(d){return d.x});
-
-    // Exit clause: Remove elements
-    //circles.exit().remove();
-
-    // Enter clause: add new elements
-    //
-    circles.enter().append("circle")
-        .attr("class", "circle")
-        .attr("x", function(d) { return x(d.x); })
-        .attr("y", function(d) { return y(d.y); })
-        .attr("fill", "none")
-        .attr("stroke", "black");
-
+    xScale.domain([0, d3.max(data, d => d.x)]);
+    yScale.domain([0, d3.max(data, d => d.y)]);
+    g.select(".x.axis").transition().duration(1000).call(xScale);
+    g.select(".y.axis").transition().duration(1000).call(yScale);
 }
 
-function redraw(data) {
-    updateXScaleDomain(data);
-    updateYScaleDomain(data);
-    updateAxes();
-    updateDrawing(data);
+// 2 - Creating a rectangle for each row
+const render = data => {
+
+    console.log("Data rendering");
+    // D3 data JOIN
+    g.selectAll('circle').data(data)
+        .join('circle')
+            .attr('id', (d, i) => i)
+            .attr('r', circleRadius)
+            .attr('cx', d => xScale(d.x))
+            .attr('cy', d => yScale(d.y))
+        .on('click', function(d,i){ updateDatasetsExceptPosition(i);  })
 }
 
-d3.json("data.json")
-	.then(function(data) {
+function updateDatasetsExceptPosition(position){
 
-    	// Drawing axes and initial drawing
-        updateYScaleDomain(data);
-        updateXScaleDomain(data);
-        drawAxes();
-    	updateDrawing(data);
+    datasets.forEach( (d,i) => {
+        if(i != position){
+            temp = d.x;
+            d.x = d.y;
+            d.y = temp;
+        }
+    });
 
-    	// var counter = 0;
-    	// setInterval(function(){
-       	// 	if (data[counter+1]){
-        //    		counter++;
-        //    		redraw(data[counter]);
-       	// 	}
-    	// }, delayTime)
-   	})
-	.catch(function(error) {
-		console.log(error); // Some error handling here
-  	});
+    console.log(datasets);
+    updateDomainAndAxis(datasets);
+    render(datasets);
+
+    // // Update Datasets Values
+    // setTimeout( () => { 
+    //     xScale.domain(0, d3.max(datasets, datasets.x));
+    //     yScale.domain(0, d3.max(datasets, datasets.y));
+    //     render(datasets);
+    // }, 1000)
+}
+
+d3.json('data.json').then(data => {
+
+    datasets = data;
+    setDomainAndAxis(datasets);
+    render(datasets);
+});
